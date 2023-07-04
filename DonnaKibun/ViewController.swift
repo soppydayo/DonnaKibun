8d850700e341cc08b2bfa99269e79dd4af3c07ce
SotaShirakawa <souta060614@gmail.com>
2023-07-04 17:46:07 +0900

    Initial Commit

---
+++ DonnaKibun/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DonnaKibun
+//
+//  Created by 白川創大 on 2023/07/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
